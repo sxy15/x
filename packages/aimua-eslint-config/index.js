69db362896ad72e4f93fb36c3d0455d3f0a1ee68
sxy15 <131671708+sxy15@users.noreply.github.com>
2023-10-19 08:37:15 +0000

    feat: init

--- packages/aimua-eslint-config/index.js
+++ packages/aimua-eslint-config/index.js
@@ -1,55 +1,61 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    ecmaVersion: 2019,
+    ecmaVersion: 2020,
     sourceType: 'module',
     extraFileExtensions: ['.vue'],
   },
-
   plugins: ['@typescript-eslint'],
-
   env: {
     es6: true,
     node: true,
-    jest: true,
     browser: true,
   },
-
   rules: {
+    'max-classes-per-file': 'off',
+    'no-new': 'off',
+    'no-shadow': 'off',
+    'no-bitwise': 'off',
+    'func-names': 'off',
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    'default-case': 'off',
+    'prefer-template': 'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'no-nested-ternary': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': 'off',
+    'no-restricted-globals': 'off',
+    'no-use-before-define': 'off',
+    'no-await-in-loop': 'off',
+    'class-methods-use-this': 'off',
+    'global-require': 'off',
     'prefer-destructuring': ['error', { object: true, array: false }],
-    // eslint-plugin-vue
+    'import/order': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-dynamic-require': 'off',
+    'vue/comment-directive': 'off',
     'vue/no-v-html': 'off',
     'vue/attributes-order': 'off',
-    'vue/require-v-for-key': 'off',
     'vue/require-default-prop': 'off',
-    'vue/no-unused-components': 'off',
+    'vue/require-explicit-emits': 'off',
     'vue/multi-word-component-names': 'off',
-    'vue/no-setup-props-destructure': 'off',
-    'vue/return-in-computed-property': 'off',
-    // typescript-eslint
+    'vue/valid-next-tick': 'off',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-extra-semi': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
   },
-
-  overrides: [
-    {
-      files: ['*.vue'],
-      parser: require.resolve('vue-eslint-parser'),
-    },
-    {
-      files: ['**/*.md/*.js', '**/*.md/*.ts'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
-      },
-    },
-  ],
 }
