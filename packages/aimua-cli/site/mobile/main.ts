fa76ab8950b6bf39867e8bace3f066494c7a776b
sxy15 <sxy_19921115@163.com>
2023-10-30 22:04:32 +0800

    feat: site

---
+++ packages/aimua-cli/site/mobile/main.ts
@@ -0,0 +1,60 @@
+import routes from '@mobile-routes'
+import config from '@config'
+import App from './App.vue'
+import Varlet from '@varlet/ui'
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { get } from 'lodash-es'
+import { inIframe, isPhone } from '../utils'
+
+import '@varlet/touch-emulator'
+import '@varlet/ui/es/style'
+
+const redirect = get(config, 'mobile.redirect')
+const defaultLanguage = get(config, 'defaultLanguage')
+
+redirect &&
+  routes.push({
+    path: '/:pathMatch(.*)',
+    redirect,
+  })
+
+routes.push({
+  path: '/home',
+  component: () => import('./AppHome.vue'),
+})
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  scrollBehavior: (to: any, from: any, savedPosition: any) => savedPosition || { left: 0, top: 0 },
+  routes,
+})
+
+router.beforeEach((to: any) => {
+  const { path, hash, query } = to
+  const language = query.language ?? defaultLanguage
+  const {replace} = query
+
+  if (!isPhone() && !inIframe()) {
+    window.location.href = `./#/${language}${path}`
+  }
+
+  if (!isPhone() && inIframe()) {
+    try {
+      // @ts-ignore
+      window.parent.onMobileRouteChange(path, language, replace, hash)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  // @ts-ignore
+  if (window._hmt) {
+    if (to.path) {
+      // @ts-ignore
+      window._hmt.push(['_trackPageview', `/#${to.fullPath}`])
+    }
+  }
+})
+
+createApp(App).use(router).use(Varlet).mount('#app')
--- packages/aimua-cli/site/pc/App.vue
+++ packages/aimua-cli/site/pc/App.vue
@@ -1,5 +1,43 @@
+<script lang="ts">
+import config from '@config'
+import { defineComponent, onMounted, ref } from 'vue'
+import { getPCLocationInfo } from '@varlet/cli/client'
+import { isPhone } from '../utils'
+import { get } from 'lodash-es'
+
+export default defineComponent({
+  setup() {
+    const useMobile = ref(get(config, 'useMobile'))
+    const defaultLanguage = get(config, 'defaultLanguage')
+
+    const init = () => {
+      const { language, menuName } = getPCLocationInfo()
+
+      if (isPhone() && useMobile.value) {
+        window.location.href = `./mobile.html#/${menuName}?language=${language || defaultLanguage}&platform=mobile`
+        
+      }
+    }
+
+    onMounted(init)
+  },
+})
+</script>
+
 <template>
-  <div></div>
+  <router-view></router-view>
 </template>
 
-<script setup lang="ts"></script>
+<style lang="less">
+body {
+  transition:
+    background-color 0.25s,
+    box-shadow 0.25s;
+
+  * {
+    transition:
+      background-color 0.25s,
+      box-shadow 0.25s;
+  }
+}
+</style>
