6b4c53c122a945d133ef812ba8966e410461ae0c
sxy15 <sxy_19921115@163.com>
2023-11-01 22:55:25 +0800

    feat: function

--- packages/aimua-shared/src/function.ts
+++ packages/aimua-shared/src/function.ts
@@ -1,40 +1,4 @@
-export const debounce = (fn: any, delay = 0) => {
-  let timer: number
+import debounce from 'lodash-es/debounce'
+import throttle from 'lodash-es/throttle'
 
-  return function (this: unknown, ...args: any[]) {
-    if (timer) {
-      clearTimeout(timer)
-    }
-
-    timer = window.setTimeout(() => {
-      fn.apply(this, args)
-    }, delay)
-  }
-}
-
-export const throttle = (fn: any, delay = 200): (() => void) => {
-  let timer: number
-  let start = 0
-
-  return function loop(this: unknown, ...args) {
-    const now = Date.now()
-    const elapsed = now - start
-
-    if (!start) {
-      start = now
-    }
-
-    if (timer) {
-      window.clearTimeout(timer)
-    }
-
-    if (elapsed >= delay) {
-      fn.apply(this, args)
-      start = now
-    } else {
-      timer = window.setTimeout(() => {
-        loop.apply(this, args)
-      }, delay - elapsed)
-    }
-  }
-}
+export { debounce, throttle }
--- packages/aimua-shared/src/index.ts
+++ packages/aimua-shared/src/index.ts
@@ -1,2 +1,4 @@
 export * from './utils.js'
 export * from './array.js'
+export * from './function.js'
+export * from './elements.js'
