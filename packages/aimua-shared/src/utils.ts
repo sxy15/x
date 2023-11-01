85631fd1a101f13e7a0a711f288eef7fc6759b9e
sxy15 <sxy_19921115@163.com>
2023-11-01 23:05:12 +0800

    feat: number

--- packages/aimua-shared/src/utils.ts
+++ packages/aimua-shared/src/utils.ts
@@ -3,3 +3,9 @@ export const { isArray } = Array
 export const isBrowser = typeof window !== 'undefined'
 
 export const isWindow = (val: unknown): val is Window => val === window
+
+export const isString = (val: unknown): val is string => typeof val === 'string'
+
+export const isBoolean = (val: unknown): val is boolean => typeof val === 'boolean'
+
+export const isNil = (val: unknown): val is null | undefined => val === null || val === undefined
