6af76af7d432f2b22355abaf16da7587b467ec52
sxy15 <sxy_19921115@163.com>
2023-11-04 21:24:39 +0800

    WIP: markdown

--- packages/aimua-vite-plugins/src/markdown.ts
+++ packages/aimua-vite-plugins/src/markdown.ts
@@ -1,12 +1,42 @@
 // import markdownIt from 'markdown-it'
 // import hljs from 'highlight.js'
-// import { kebabCase } from "@aimua/shared"
+import { kebabCase } from '@aimua/shared'
 // import { pinyin } from 'pinyin-pro'
 import type { Plugin } from 'vite'
 
+function extractComponents(source: string) {
+  const componentRE = /import (.+) from ['"].+['"]/
+  const importRE = /import .+ from ['"].+['"]/
+  const vueRE = /```vue((.|\n)*?)```/g
+  const components: string[] = []
+  const imports: string[] = []
+
+  source = source.replace(vueRE, (match, p1) => {
+    const partImport = p1.match(importRE)
+
+    const partComponents = partImport?.map((importer: string) => {
+      importer = importer.replace(/(\n|\r)/g, '')
+      const component = importer.replace(componentRE, '$1')
+      !imports.includes(importer) && imports.push(importer)
+      !components.includes(component) && components.push(component)
+
+      return `<${kebabCase(component)} />`
+    })
+
+    return partComponents ? `<div class="aimua-component-preview">${partComponents.join('\n')}</div>` : ''
+  })
+
+  return {
+    source,
+    imports,
+    components,
+  }
+}
+
 function markdownToVue(source: string, options: MarkdownOptions) {
-  console.log(source)
-  console.log(options)
+  const { source: vueSource, imports, components } = extractComponents(source)
+
+  console.log(vueSource, imports, components, options)
 }
 
 export interface MarkdownOptions {
